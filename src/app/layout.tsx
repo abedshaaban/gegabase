62a9756e3e05698c1097c5e7c9fc10304de27f66
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-07-15 14:19:11 +0300

    revalidate false

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export const metadata: Metadata = {
   manifest: `${siteConfig.url}/site.webmanifest`,
 }
 
-export const dynamic = 'force-dynamic'
+export const revalidate = false
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   return (
