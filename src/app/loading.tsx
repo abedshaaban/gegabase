ad2732a64507f7a15ae1517028adc89b575b029c
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-07-15 14:14:37 +0300

    added loading page for suspenese

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="w-full bg-white h-auto min-h-screen pt-16 px-4 pb-8 absolute top-0 flex flex-col justify-center items-center gap-9 z-[999]">
+      loading...
+    </div>
+  )
+}
